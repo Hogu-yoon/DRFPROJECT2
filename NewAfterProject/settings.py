a8f8df10b865c434a9df7d7ab2bb9cb1595e0e86
asd <pa6712@naver.com>
2022-06-17 19:27:51 +0900

    project 2

--- NewAfterProject/settings.py
+++ NewAfterProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from NewAfterProject.local_settings import DJANGO_SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-59^0cr0cqk$z8h$0!em5)6eg#)wwm^42ki#)yje5v5(46e9c#c'
+SECRET_KEY = DJANGO_SECRET['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
